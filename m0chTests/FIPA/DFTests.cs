b2a24cd379b9a5eb1f4d0d61b041bc93e9aea95c
Tiago Neto <ei12109@fe.up.pt>
2017-11-09 19:57:37 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ m0chTests/FIPA/DFTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using m0ch.FIPA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m0ch.FIPA.Tests
+{
+    [TestClass()]
+    public class DFTests
+    {
+        [TestMethod()]
+        public void DFTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            // Creating Directory Facilitator
+            DF directoryF = new DF("for");
+
+            // Creating an agent
+            AID tryNewAgent = new AID("test", "purposes");
+
+            // register agent on yellow pages
+            directoryF.Register(tryNewAgent);
+
+            // Search for the agent
+            AID searchedAgent = directoryF.Search("test");
+
+            Assert.Equals(tryNewAgent, searchedAgent);
+        }
+
+        [TestMethod()]
+        public void deregisterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void modifyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void searchTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
