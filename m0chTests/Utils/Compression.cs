ac93a78683f1e8e05bf03c39757e1455e42c3672
Cowtard <ei12109@fe.up.pt>
2017-11-09 16:12:07 +0000

    Added some tests.

--- m0chTests/Utils/Compression.cs
+++ m0chTests/Utils/Compression.cs
@@ -8,13 +8,25 @@ namespace m0chTests
     public class Compression
     {
         [Test()]
-        public void GZIPCompression()
+        public void Test_GZIP_Compression()
         {
 
             GZIP test = new GZIP("Test");
 
-              
+            byte[] compressedData = test.CompressData();
+            
+            Assert.AreNotEqual(compressedData, null);
+        }
+        
+        [Test()]
+        public void Test_GZIP_Compression_Decompression()
+        {
 
+            GZIP test = new GZIP("Test");
+
+            byte[] compressedData = test.CompressData();
+            
+            Assert.AreEqual("Test", test.DecompressData(compressedData));
         }
     }
 }
