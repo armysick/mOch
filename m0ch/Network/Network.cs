0410f1d31b01cf4b46b27d99da5f9080ebd536ab
Cowtard <ei12109@fe.up.pt>
2017-11-09 14:28:51 +0000

    Change to force casting function to actually wait for the dequeue.

--- m0ch/Network/Network.cs
+++ m0ch/Network/Network.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.IO;
 using m0ch.Utils;
 using System.Threading;
@@ -26,9 +27,11 @@ namespace m0ch.Network
         /// Variable that stores all the messages that failed to be sent
         /// </summary>
         private System.Collections.Concurrent.ConcurrentQueue<MessageContainer> resendInbox; 
-            
-            
-        // Object representing server
+        
+        
+        /// <summary>
+        /// Object that handles the listening part of the platform
+        /// </summary>
         private Server listeningServer;
 
         /// <summary>
@@ -44,27 +47,70 @@ namespace m0ch.Network
             resendInbox = new ConcurrentQueue<MessageContainer>();
 
             listeningServer = new Server(Listeningport, ref untreatedInbox);
-            start();
+            Start();
 
         }
 
         /// <summary>
         /// Initializes the server's thread
         /// </summary>
-        public void start()
+        private void Start()
         {
             Thread serverThread = new Thread(listeningServer.RunServer);
             serverThread.Start();
+            
+            
+            Thread castingThread = new Thread(CastingMessages);
+            castingThread.Start();
             
         }
 
         /// <summary>
         /// Function responsible for making the server stop listening
         /// </summary>
-        public void stopServer()
+        public void StopServer()
         {
             listeningServer.stopServer();
 
+        }
+
+
+        /// <summary>
+        /// This function runs in a thread and is responsible for retrieve each message from untreatedInbox and 
+        /// converting it a message object and store it in treatedInbox.
+        /// </summary>
+        private void CastingMessages()
+        {
+
+            while (true)
+            {
+
+                if (untreatedInbox.Count > 0)
+                {
+                    try
+                    {
+                        Message receivedMessage;
+                        byte[] receivedBytes;
+
+
+                        while (!untreatedInbox.TryDequeue(out receivedBytes))
+                        {};
+                        
+                        // Added a useless string instead of the message for now
+                        // TODO: Serialization of messages to better decoding
+                        
+                        treatedInbox.Enqueue(new Message(Perfomative.FAILURE));
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                
+            }
+            
+            
         }
     }
 }
