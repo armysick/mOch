7d5790cc5aee884a0eb79a7115a90de138eb45e6
Cowtard <ei12109@fe.up.pt>
2017-11-10 11:49:29 +0000

    Please compile tests.

--- m0chTests/Properties/AssemblyInfo.cs
+++ m0chTests/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("m0ch")]
+[assembly: AssemblyTitle("m0chTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
