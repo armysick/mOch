3f866b0f395da0fc24cb7ae19289621dfb7429dd
Cowtard <ei12109@fe.up.pt>
2017-11-10 14:02:49 +0000

    Created AbstractAgent as everything in a Plataform is an agent.

---
+++ m0ch/Agents/AbstractAgent.cs
@@ -0,0 +1,27 @@
+﻿
+namespace m0ch.Agents
+{
+    
+    /// <summary>
+    /// Class that represent every agent except for the cluster
+    /// </summary>
+    abstract class AbstractAgent
+    {
+        
+        /// <summary>
+        /// Variable that holds everything that happens in the network
+        /// </summary>
+        protected Network.Networking NetworkAcess;
+
+
+        /// <summary>
+        /// Method that returns Network variable
+        /// </summary>
+        /// <returns></returns>
+        protected Network.Networking GetNetworking()
+        {
+            return this.NetworkAcess;
+        }
+
+    }
+}
--- m0ch/Agents/MainCluster.cs
+++ m0ch/Agents/MainCluster.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using m0ch.FIPA;
 using m0ch.Network;
 
@@ -12,13 +8,8 @@ namespace m0ch.Agents
     /// <summary>
     /// Class that represents the FIPA cluster
     /// </summary>
-    class MainCluster
+    class MainCluster : AbstractAgent
     {
-
-        /// <summary>
-        /// Variable that holds everything that happens in the network
-        /// </summary>
-        private Network.Networking _networkAcess;
         
         /// <summary>
         /// Variable that holds and is used to interact with AMS and DF
@@ -37,7 +28,7 @@ namespace m0ch.Agents
         /// <param name="serverPort"></param>
         public MainCluster(int serverPort)
         {
-            this._networkAcess = new Networking(serverPort);
+            this.NetworkAcess = new Networking(serverPort);
             this._services = new AMServices();
 
             GotNewMessage += GotMessageEvent;
